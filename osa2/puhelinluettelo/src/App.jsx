72b2e23860f1d0b15cadb192723016b7cb5595ca
Ruchi <ruchishah16081991@gmail.com>
2026-06-22 21:11:53 +0300

    Added axios, db.json and useEffect in phone list

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './Components/Persons.jsx'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(reponse => {
 
+      setPersons(reponse.data)
+    })
+  }
+
+  useEffect(hook, [])
 
   const onSubmit = (e) => {
     e.preventDefault()
